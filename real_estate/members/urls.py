877bd65d0e707eb539d8fbecdaf31f4c13c10665
Dev Agrawal <165568263+Dev0Agrawal@users.noreply.github.com>
2024-10-30 03:46:17 +0530

    Update urls.py

--- real_estate/members/urls.py
+++ real_estate/members/urls.py
@@ -1,10 +1,13 @@
 # members/urls.py
 from django.urls import path
-from .views import home_view, login_view, property_list_view,signup_view
+from .views import home_view, login_view, property_list_view,signup_view, profile_view,logout_view,dashboard_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('login/', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
+    path('profile/', profile_view, name='profile'),
     path('properties/', property_list_view, name='property_list'),
+    path('logout/', logout_view, name='logout'),
+    path('dashboard/', dashboard_view, name='dashboard'),
 ]
